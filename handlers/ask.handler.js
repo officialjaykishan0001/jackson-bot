af866b39fe3f9862e86a08f31e926da0a4d04f93
Jay kishan kharwar <jaykishankharwar215@gmail.com>
2025-07-11 14:04:08 +0530

    bug fix

--- handlers/ask.handler.js
+++ handlers/ask.handler.js
@@ -1,11 +1,19 @@
-const askAI = require('../services/ai.service');
+const askAI = require("../services/ai.service");
 
 module.exports = async function askHandler(bot, msg, match) {
   const chatId = msg.chat.id;
+
+  if (!match[1]) {
+    return bot.sendMessage(
+      msg.chat.id,
+      "❗ Please provide a question after `/ask`."
+    );
+  }
+  
   const question = match[1];
 
-  await bot.sendChatAction(chatId, 'typing');
-  bot.sendMessage(chatId, 'Thinking.')
+  await bot.sendChatAction(chatId, "typing");
+  bot.sendMessage(chatId, "Thinking.");
   const answer = await askAI(question);
-  bot.sendMessage(chatId, answer, { parse_mode: 'Markdown' });
+  bot.sendMessage(chatId, answer, { parse_mode: "Markdown" });
 };
